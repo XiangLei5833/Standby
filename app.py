008990ec4c5e562e7918317aa37d5e91365d7a01
雷翔 <leixiang5833@163.com>
2017-12-01 11:44:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,43 +1,85 @@
-import os
-import json
 from flask import Flask, render_template, abort
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root@localhost/shiyanlou'
+db = SQLAlchemy(app)
+client = MongoClient('127.0.0.1, 27017')
+db.client = client.shiyanlou
 
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    content = db.Column(db.Text)
+    created_time = db.Column(db.DateTime)
+    category = db.relationship('Category', backref = db.backref('files'))
+    
+    def __init__(self, title,  category, content, created_time=None):
+        self.title = title
+        self.category = category
+        self.content = content
+        if created_time is None:
+            created_time = datetime.utcnow()
+        self.created_time = created_time
+    def __repr__(self):
+        return '<File %s>' % self.title
+
+    def add_tag(self, tag_name):
+        file_tag = {'file_id': self.id, 'tag_name': tag_name}
+        db_client.tag.insert_one(file_tag)
+
+    def remove_tag(self, tag_name):
+        delete_tag = db_client.tag.find_one({'file_id': self.id})
+        if 'tag_name' in delete_tagvalues():
+            db_client.tag.delete_one({'file_id': self.id})
+        else:
+            print('tag_name not been found')
+
+    def tags(self):
+        find_tag = db_client.tag.find_one({'file_id': self.id})
+        tag_list = list(find_tag['tag_name'])
+        return tag_list
+
+            
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80))
+    
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return '<Category %s>' % self.name
+
+titles = File.query.all()
+file_tag = File()
+tag_list = file_tag.tags()
+news1 = titles[0]
+news2 = titles[1]
 @app.route('/')
 def index():
-    path0 = 'home/shiyanlou/files'
-    filename = os.listdir(path0)
-    file1_path = os.path.join(path0, filename[0])
-    file2_path = os.path.join(path0, filename[1])
-    with open(file1_path, 'r') as papers1:
-        file_content1 = json.loads(papers1.read())
-    with open(files_path, 'r') as papers2:
-        file_content2 = json.loads(papers2.read())
-    title = {
-            title1: file_content1['title']
-            title2: file_content2['title']
+    return render_template('index.html',titles=titles, tag_list=tag_list)
+
+@app.route('/files/<file_id>')
+def file(file_id):
+    file_content = {
+            'title1': news1.title,
+            'title2': news2.title,
+            'created_time1': news1.created_time,
+            'created_time2': news2.created_time,
+            'content1': news1.content,
+            'content2': news2.content,
+            'type1': news1.category,
+            'type2': news2.category
             }
+    return render_template('file.html', file_content=file_content, file_id=file_id, titles=titles)
 
-    return render_template('index.html', title=title)
-
-@app.route('files/<filename>')
-def file(filename):
-    index_instance = index()
-    if os.path.isfile(index_instance.file1_path) and os.path.isfile(index_instance.file2_path):
-        file_content = {
-                title1: index_instance.file_content1['title']
-                title1: index_instance.file_content1['title']
-                created_time1: index_instance.file_content1['created_time']
-                created_time2: index_instance.file_content1['created_time']
-                content1: index_instance.file_content1['content']
-                content2: index_instance.file_content1['content']
-                }
-        return render_template('file.html', filename=filename)
-    else:
-        abort(404)
-        return render_template('404.html', filename=filename)
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'),404
     
 
 if __name__ == '__main__':
